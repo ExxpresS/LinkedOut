e8fd0fb64b6600c4e2b8c716182f030e2197f1cc
PierreN <pnolot@orange.fr>
2019-06-20 02:47:53 +0200

    form poster annonce recrutement ecrasé et mercé

--- View/php/pages/Poster.php
+++ View/php/pages/Poster.php
@@ -1,16 +1,18 @@
 <?php
 session_start();
-    include $_SERVER['DOCUMENT_ROOT'] . '/View/php/template/HeaderUnLog.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/View/php/template/Header.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/Controller/GetUsersInfos.php';
 ?>
 
 
 <body>
     <?php
+    var_dump("disparition du code j'ai plus envie");
         include $_SERVER['DOCUMENT_ROOT'] . '/View/php/template/NavBar.php';
-        include $_SERVER['DOCUMENT_ROOT'] . '/View/php/template/LeftMenu.php';
-        $title = "olivier";
+        //include $_SERVER['DOCUMENT_ROOT'] . '/View/php/template/LeftMenu.php';
+        $title = "Poster";
         
     ?>
+    
     
 </body>
